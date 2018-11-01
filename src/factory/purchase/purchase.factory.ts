e4d975330fcd72b7c63b282fe84f4ba7ef7d1341
hayato.osada <hayato.osada@briswell.com>
2018-11-01 17:49:09 +0900

    MWSR-244 package修正

---
+++ src/factory/purchase/purchase.factory.ts
@@ -0,0 +1,147 @@
+/**
+ * purchase.factory
+ */
+export interface ITempSettlementRegistArgs {
+    /**
+     * 決済管理番号（16桁固定）
+     */
+    kssiknrNo: string;
+    /**
+     * 購入者 姓（最大15文字）
+     */
+    knyshsiNm: string;
+    /**
+     * 購入者 名（最大15文字）
+     */
+    knyshmiNm: string;
+    /**
+     * 購入者メールアドレス（最大128文字）
+     */
+    knyshMladdr: string;
+    /**
+     * 購入者市外局番（最大5文字）
+     */
+    knyshshgikykNo: string;
+    /**
+     * 購入者市内局番（最大4文字）
+     */
+    knyshshnikykNo: string;
+    /**
+     * 購入者加入者番号（最大4文字）
+     */
+    knyshknyshNo: string;
+    /**
+     * 購入日時（日時形式 format: yyyy/MM/dd HH:mm:ss）
+     */
+    knyDt: string;
+    /**
+     * 販売チャネル区分（2桁固定）
+     */
+    hmbichnnlTyp: string;
+    /**
+     * 販売会社コード（6桁固定）
+     */
+    hmbigishCd: string;
+    /**
+     * 購入デバイス区分（2桁固定）
+     */
+    knydvcTyp: string;
+    /**
+     * 購入者コード（会員の場合は 8桁の固定値 を それ以外は null をセット）
+     */
+    knyshCd: string;
+    /**
+     * 会員フラグ（会員の場合は 1 を それ以外は null または 0 をセット）
+     */
+    kiinFlg: string;
+    /**
+     * 作品コード（6桁固定）
+     */
+    skhnCd: string;
+    /**
+     * "鑑賞券管理番号（8桁固定）
+     */
+    knshknknrNo: string;
+    /**
+     * 鑑賞券情報
+     */
+    knshknInf: {};
+    /**
+     * 決済方法区分_22: 楽天ペイ）
+     */
+    kssihhTyp: string;
+    /**
+     * 併用決済フラグ（ギフトカード利用時は 1 をそれ以外は null をセット）
+     */
+    hiykssiFlg: string;
+    /**
+     * 併用決済方法区分（ギフトカード利用時は 04 を それ以外は null をセット）
+     */
+    hiykssihhTyp: string;
+    /**
+     * 併用決済利用合計金額
+     */
+    hiykssirygkiknGk: number;
+    /**
+     * ギフトカード情報（ギフト利用がない場合は null をセット）
+     */
+    gftInf: {};
+    /**
+     * ポイント利用フラグ（ポイント利用時は 1 をそれ以外は null または 0 をセット）
+     */
+    pintryFlg: string;
+    /**
+     * ポイント決済UUID
+     */
+    pintkssiUuid: string;
+    /**
+     * 利用ポイント
+     */
+    ryPt: number;
+    /**
+     * 獲得ポイント
+     */
+    kktkPt: number;
+    /**
+     * 利用金額（ギフトカード、ポイント利用分を差引いた利用金額）
+     */
+    ryknGk: number;
+    /**
+     * 合計金額（購入チケットの合計金額）
+     */
+    gkknGk: number;
+    /**
+     * メールテンプレートコード（5桁固定）
+     */
+    mltmpltCd: string;
+    /**
+     * 特典情報
+     */
+    tktncdkkhInf: {};
+}
+
+export interface IInfoArgs {
+    /**
+     * 決済管理番号（16桁固定）
+     */
+    kssiknrNo: string;
+}
+
+export interface IInfoResult {
+    /**
+     * 購入管理番号
+     */
+    knyknrNo: string;
+    /**
+     * QRコードURL
+     */
+    qrcdUrl: string;
+    /**
+     * デジタルインセンティブダウンロードURL
+     */
+    dgtlincntvdwnlodgmnUrl: string;
+    /**
+     * 獲得ポイント
+     */
+    kktkPt: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import * as pointUtil from './utils/point/point.util';
 
 import { PointService } from './service/point/point.service';
 import { UserService } from './service/user/user.service';
+import { PurchaseService } from './service/purchase/purchase.service';
 
 export import factory = factory;
 
@@ -26,6 +27,11 @@ export namespace service {
      * @class
      */
     export class Point extends PointService { }
+    /**
+     * 決済関連サービス
+     * @class
+     */
+    export class Purchase extends PurchaseService { }
 }
 
 export namespace utils {
