fc24c5f96d0ab8534e69ecd38767a3834a820805
Gia Thanh <thanh.clg@briswell.com>
2019-05-17 08:45:58 +0700

    ###changed fix file name

--- src/factory.ts
+++ src/factory.ts
@@ -3,8 +3,10 @@
  * @namespace factory
  */
 
+import * as EigagiftFactory from './factory/eigagift/eigagift.factory';
 import * as PointFactory from './factory/point/point.factory';
 import * as PurchaseFactory from './factory/purchase/purchase.factory';
 
 export import point = PointFactory;
 export import purchase = PurchaseFactory;
+export import eigagift = EigagiftFactory;
