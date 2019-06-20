9ef44cacadb837ad3853eb45b76b315267b02e85
Toshiharu Tanaka <toshiharu.tanaka@briswell.com>
2019-06-20 14:20:20 +0900

    MWSR-278 49 対応劇場情報取得

--- src/service/theater/theater.service.ts
+++ src/service/theater/theater.service.ts
@@ -64,7 +64,7 @@ export class TheaterService extends Service {
                     stNm: stInfo.st_nm,
                     stRmk: stInfo.st_rmk,
                     mdgchryknFlg: stInfo.mdgchrykn_flg,
-                    ntryknFlg: stInfo.ntrykn_flg,
+                    ntryknFlg: stInfo.ntrykn_flg
                 };
             })
         };
